d4c889b1e999bf4934b9bf6127e55e6dc4a16064
comason <comason@jackhenry.com>
2016-01-12 16:40:54 +0000

    working flask

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from flask import Manager
+import os
+from flask import Flask
 from flask import render_template
 from flask import request, redirect, url_for
 
@@ -83,22 +84,27 @@ def delete_entry(entryid):
 def login_get():
     return render_template("login.html")
 
-#from flask import flash
-#from flask.ext.login import login_user
-#from werkzeug.security import check_password_hash
-#from .database import User
-#
-#@app.route("/login", methods=["POST"])
-#def login_post():
-#    email = request.form["email"]
-#    password = request.form["password"]
-#    user = session.query(User).filter_by(email=email).first()
-#    if not user or not check_password_hash(user.password, password):
-#        flash("Incorrect username or password", "danger")
-#        return redirect(url_for("login_get"))
-#
-#    login_user(user)
-#    return redirect(request.args.get('next') or url_for("entries"))
+from flask import flash
+from flask.ext.login import login_user
+from flask.ext.login import login_required
+from werkzeug.security import check_password_hash
+from .database import User
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
     
 
+
+    
+    
+    
 
